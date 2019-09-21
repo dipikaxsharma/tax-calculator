36be218dd3cfe9e2e9666f949e6f10692535117f
bomma.anil <anil.bommaa@gmail.com>
2019-09-21 16:01:32 -0500

    updated

--- calculator.js
+++ calculator.js
@@ -3,8 +3,9 @@ function updateInput() {
     document.getElementById("tip").innerText = tip + ".00";
 }
 
-function calculateRate () {
+function calculateRate() {
     let total = parseInt(document.querySelector('#total').value);
     let tip = parseFloat(document.querySelector('#tipAmount').value);
-    document.getElementById("grandTotal").innerText = tip + total + ".00";
+    let tipRate = parseFloat(document.querySelector('#tax').value);
+    document.getElementById("grandTotal").innerText = tip + total + tipRate + ".00";
 }
