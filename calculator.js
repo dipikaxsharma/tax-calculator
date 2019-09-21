c75385a132cd4d0584bdc97a186c843852539ce7
bomma.anil <anil.bommaa@gmail.com>
2019-09-21 16:07:20 -0500

    updated

--- calculator.js
+++ calculator.js
@@ -6,6 +6,5 @@ function updateInput() {
 function calculateRate() {
     let total = parseInt(document.querySelector('#total').value);
     let tip = parseFloat(document.querySelector('#tipAmount').value);
-    let tipRate = parseFloat(document.querySelector('#tax').value);
-    document.getElementById("grandTotal").innerText = tip + total + tipRate + ".00";
+    document.getElementById("grandTotal").innerText = tip + total + 5.50;
 }
