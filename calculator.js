5ca2d1a0e652a2e768eb10e166c28dd50752a309
bomma.anil <anil.bommaa@gmail.com>
2019-09-21 19:42:28 -0500

    updated

--- calculator.js
+++ calculator.js
@@ -4,12 +4,14 @@ function updateInput() {
     let total = parseInt(document.querySelector('#total').value);
     tip = total * document.querySelector('#tipAmount').value;
     tip /= 100;
-    document.getElementById("tipRate").innerHTML = tip;
+    if (total)
+        document.getElementById("tipRate").innerHTML = tip;
 }
 
 function calculateRate() {
-    if (tip) {
+    if (tip >= 0) {
         let total = parseInt(document.querySelector('#total').value);
         document.getElementById("grandTotal").innerText = tip + total + 5.50;
+        event.preventDefault();
     }
 }
